644c5a71a8ddce94c052999782ff8bb605ddac2b
lloydsi <lloydsi66@gmail.com>
2016-12-29 13:09:46 +0000

    added category ids and centred map --insert--

--- js/script1.js
+++ js/script1.js
@@ -13,11 +13,12 @@
 document.addEventListener('DOMContentLoaded', function(){
 	console.log("Dom loaded")
 	
-	function MapLoc(){	
-    var xhttpMapLoc = new XMLHttpRequest();    /*Attempting to geolocate input*/
+	function MapLoc(){
+        var mapKey = "AIzaSyAweZVmZtaw8bYWarIUvRM5WT4NDgD9BP8"		
+        var xhttpMapLoc = new XMLHttpRequest();    /*Attempting to geolocate input*/
         xhttpMapLoc.addEventListener('load', processResponseMapLoc);
-		var add = document.getElementById("location_input").value;
-        xhttpMapLoc.open("GET", "https://maps.googleapis.com/maps/api/geocode/json?address="+add+"&key=AIzaSyAweZVmZtaw8bYWarIUvRM5WT4NDgD9BP8");           
+		var add = document.getElementById("location_input").value;  /*address location from input box*/
+        xhttpMapLoc.open("GET", "https://maps.googleapis.com/maps/api/geocode/json?address="+add+"&key="+mapKey+"&region=uk");           
 	    xhttpMapLoc.send();
 		
 }
@@ -27,20 +28,20 @@ document.addEventListener('DOMContentLoaded', function(){
         var dataLoc = JSON.parse(this.response);
 	    console.log(dataLoc.results[0].geometry.location.lat + "," + dataLoc.results[0].geometry.location.lng);   /*get the lat and long of the place*/
 		var ll = document.getElementById("ll")
-		var ll = ll.value=dataLoc.results[0].geometry.location.lat + "," + dataLoc.results[0].geometry.location.lng;
-		var lim = 30;    /*limit of results*/
-		var radius = 1000; /*search radius*/
-		console.log("ll is"+ll);
+		var ll = (ll.value=dataLoc.results[0].geometry.location.lat + "," + dataLoc.results[0].geometry.location.lng);
+		
+		console.log("ll is "+ll);
 			
 		function FsPlaces(){
             var xhttp = new XMLHttpRequest();
             xhttp.addEventListener('load', processResponse); /*use the lat and lng to search foursquare*/
-			var clientId = "PRS3ZGZSJIAI0A12KIP15VIY12ITIGFRBFVCRKRGF02AUVYW"
+			var lim = 30;    /*limit of results*/
+		    var radius = 1500; /*search radius*/
+		    var clientId = "PRS3ZGZSJIAI0A12KIP15VIY12ITIGFRBFVCRKRGF02AUVYW"
 			var clientSecret = "GL4DEJONWD12BTHKHJAJNOY5L3SYZD3MLWHIUYXMNGOHFCTT";
 			var categoryID = String(document.getElementById("activity").innerHTML);   /*string containing venue category id's from buttons*/
-            xhttp.open("GET", "https://api.foursquare.com/v2/venues/search?ll="+ll+"&client_id="+clientId+"&client_secret="+clientSecret+"&v=20170101&categoryId="+categoryID+"&limit="+lim+"&radius="+radius);            
-			/*xhttp.open("GET", "https://api.foursquare.com/v2/venues/search?ll="+ll+"&venuePhotos=1&client_id=PRS3ZGZSJIAI0A12KIP15VIY12ITIGFRBFVCRKRGF02AUVYW&client_secret=GL4DEJONWD12BTHKHJAJNOY5L3SYZD3MLWHIUYXMNGOHFCTT&v=20170101&venuePhotos=1&categoryId=4d4b7104d754a06370d81259&limit="+lim+"&radius="+radius);
-			*/xhttp.send();
+            xhttp.open("GET", "https://api.foursquare.com/v2/venues/search?ll="+ll+"&intent=browse&client_id="+clientId+"&client_secret="+clientSecret+"&v=20170101&categoryId="+categoryID+"&limit="+lim+"&radius="+radius);            
+			xhttp.send();
 			}
 			FsPlaces();
 			
@@ -49,7 +50,7 @@ document.addEventListener('DOMContentLoaded', function(){
             console.log(data.response);
 			var para=document.getElementById("placestogo");
 			var ulist=para.appendChild(document.createElement("ol"));
-			var startCenter={lat:Number(data.response.venues[0].location.lat), lng:Number(data.response.venues[0].location.lng)}
+			var startCenter={lat:Number(dataLoc.results[0].geometry.location.lat), lng:Number(dataLoc.results[0].geometry.location.lng)}
 			var map = new google.maps.Map(document.getElementById('map'), {zoom: 15, center:startCenter});
 			for(i=0;i < data.response.venues.length;i++ ){
 				var items = ulist.appendChild(document.createElement("li"));
@@ -63,7 +64,12 @@ document.addEventListener('DOMContentLoaded', function(){
 				var venueId= data.response.venues[i].id
 				var venueLink = 'http://foursquare.com/v/' + venueId +'?ref=PRS3ZGZSJIAI0A12KIP15VIY12ITIGFRBFVCRKRGF02AUVYW'
 				var contentString = venueName+'<div id="markerwindow"><p> <a href='+venueLink+' target="_blank">More Info...</a></p></div>'   
-				var marker = new google.maps.Marker({position: where, map: map, icon:"star.png", label: labels});
+				if (data.response.venues[i].id === "4c38df4de52ce0d596b336e1"){
+					var marker = new google.maps.Marker({position: where, map: map, icon:"parking.png", label: labels});
+				}
+				else{
+				    var marker = new google.maps.Marker({position: where, map: map, icon:"star.png", label: labels});
+				}
 				marker.content = contentString 
 				var infoWindow = new google.maps.InfoWindow();
 				google.maps.event.addListener(marker, 'click', function() {infoWindow.setContent(this.content);infoWindow.open(this.map,this)});
@@ -92,7 +98,7 @@ document.addEventListener('DOMContentLoaded', function(){
     function listen() {
 	    var go = document.getElementById("go").addEventListener("click", enter, true);
         console.log("listening");
-		/*var checked = document.getElementsByClass("myButton".checked=true).  LOOK AT CHANGING ON CHANGE*/
+		/*var checked = document.getElementsByClassName("myButton".checked=true);*/
 	}
 	
 	function checking(){
@@ -112,46 +118,55 @@ document.addEventListener('DOMContentLoaded', function(){
 		
 		if (sports === true){
 			    var sportsId = document.getElementById("sports").value
-				document.getElementById("activity").innerHTML = sportsId;
-				var activity = document.getElementById("activity").innerHTML;
+				var activity = document.getElementById("activity").innerHTML+sportsId;
+				document.getElementById("activity").innerHTML=activity;
 		}
 		
 		if (parks === true){
 			    var parksId = document.getElementById("parks").value;
 				var activity = document.getElementById("activity").innerHTML+","+parksId;
+				document.getElementById("activity").innerHTML=activity;
 				
 		}
 		if (cinema === true){
 			    var cinemaId=document.getElementById("cinema").value;
-				var activity = document.getElementById("activity").innerHTML+","+cinemaId;		
+				var activity = document.getElementById("activity").innerHTML+","+cinemaId;	
+				document.getElementById("activity").innerHTML=activity;
 		}
 		if (food=== true){
 			    var foodId=document.getElementById("food").value;
-				var activity = document.getElementById("activity").innerHTML+","+foodId;		
+				var activity = document.getElementById("activity").innerHTML+","+foodId;
+				document.getElementById("activity").innerHTML=activity;
 		}
 		if (outdoor === true){
 			    var outdoorId=document.getElementById("outdoor").value;
-				var activity = document.getElementById("activity").innerHTML+","+outdoorId;		
+				var activity = document.getElementById("activity").innerHTML+","+outdoorId;
+				document.getElementById("activity").innerHTML=activity;
 		}
 		if (sports === true){
 			    var sportsId=document.getElementById("sports").value;
-				var activity = document.getElementById("activity").innerHTML+","+sportsId;		
+				var activity = document.getElementById("activity").innerHTML+","+sportsId;
+				document.getElementById("activity").innerHTML=activity;
 		}
 		if (museum === true){
 			    var museumId=document.getElementById("museum").value;
-				var activity = document.getElementById("activity").innerHTML+","+museumId;		
+				var activity = document.getElementById("activity").innerHTML+","+museumId;	
+                document.getElementById("activity").innerHTML=activity;				
 		}
 		if (craft === true){
 			    var craftId=document.getElementById("craft").value;
-				var activity = document.getElementById("activity").innerHTML+","+craftId;		
+				var activity = document.getElementById("activity").innerHTML+","+craftId;	
+                document.getElementById("activity").innerHTML=activity;				
 		}
 		if (music === true){
 			    var musicId=document.getElementById("music").value;
-				var activity = document.getElementById("activity").innerHTML+","+musicId;		
+				var activity = document.getElementById("activity").innerHTML+","+musicId;	
+                document.getElementById("activity").innerHTML=activity;				
 		}
 		if (parking === true){
 			    var parkingId=document.getElementById("parking").value;
-				var activity = document.getElementById("activity").innerHTML+","+document.getElementById("parking").value;		
+				var activity = document.getElementById("activity").innerHTML+","+document.getElementById("parking").value;	
+                document.getElementById("activity").innerHTML=activity;				
 		}
 		
 	}  
